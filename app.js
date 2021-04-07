4b6053787d4b1a7d525d73f3ecef6720ad3d46aa
eaaninwezi <eaaninwezi@gmail.com>
2021-04-08 02:38:22 +0300

    completed

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Article = require('./models/article')
 const articlesRouter = require('./routes/articles');
+const methodOverride = require('method-override')
 const app = express();
 
-mongoose.connect('mongodb://localhost/dailyDose', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/dailyDose', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
 
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
 
 
-app.get('/', function(req, res){
-    const articles = [{
-        title: "Testing",
-        createdAt: new Date(),
-        description: "vdsfujcbvbu"
-    }]
+app.get('/', async function(req, res){
+   const articles = await Article.find().sort({
+       createdAt: 'desc'
+   })
     res.render('articles/index', {articles: articles});
 });
 
