59382b97355f04e85ca3c07527a1a252818d7532
eaaniwezi <eaaninwezi@gmail.com>
2021-06-06 19:30:00 +0300

    tete

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Article = require('./models/article')
-const articlesRouter = require('./routes/articles');
-const methodOverride = require('method-override')
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articlesRouter = require("./routes/articles");
+const methodOverride = require("method-override");
 const app = express();
 
-mongoose.connect('mongodb://localhost/dailyDose', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
-
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: false}));
-app.use(methodOverride('_method'))
-
-
-app.get('/', async function(req, res){
-   const articles = await Article.find().sort({
-       createdAt: 'desc'
-   })
-    res.render('articles/index', {articles: articles});
+mongoose.connect("mongodb://localhost/dailyDose", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.use('/articles', articlesRouter);
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.listen(3000)
+app.get("/", async function (req, res) {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articlesRouter);
+app.listen(3000);
